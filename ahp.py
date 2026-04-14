a588b8c4da5cf5d7f5fbe8bacd49f7deba6fafd2
olga toptunov <o.apiecionek@gmail.com>
2026-04-14 22:40:22 +0200

    Poprawa funkcjonalności i ui, dodanie wyłączenia slidera przy równych wagach

--- ahp.py
+++ ahp.py
@@ -18,7 +18,7 @@ criteria = [
 
 # pair generation
 pairs = [(i, j) for i in range(len(criteria)) for j in range (i+1, len(criteria))]
-state = {'index': 0, 'value': 5}
+state = {'index': 0, 'value': 2}
 answers = {}
 
 def current_pair():
@@ -31,7 +31,14 @@ def set_ui_from_state():
     label_y.set_text(y)
     step_label.set_text(f'Pytanie {state["index"] + 1}/{len(pairs)}')
     value_badge.set_text(str(state['value']))
+    if state['value'] == 1:
+        direction.value = 'Równa ważność'
+        slider.disable()
+    else:
+        slider.enable()
+
     slider.value = state['value']
+    value_badge.set_text(str(state['value']))
 
 def in_slider_change(e):
     state['value'] = int(e.value)
@@ -41,17 +48,33 @@ def in_slider_change(e):
 def go_prev():
     if state['index'] > 0:
         state['index'] -= 1
-        state['value'] = answers.get(pairs[state['index']], 5)
+        state['value'] = answers.get(pairs[state['index']], 2)
         set_ui_from_state()
 
 def go_next():
-    answers[pairs[state['value']]] = state['value']
+    answers[pairs[state['index']]] = state['value']
     if state['index'] < len(pairs) - 1:
         state['index'] += 1
-        state['value'] = answers.get(pairs[state['index']], 5)
+        state['value'] = answers.get(pairs[state['index']], 2)
         set_ui_from_state()
     else:
         ui.notify('Koniec pytań 🎉')
+
+
+# === equal importance ===
+def on_direction_change(e):
+    if e.value == 'Równa ważność':
+        state['value'] = 1
+        slider.value = 1
+        slider.disable()
+        value_badge.set_text('1')
+    else:
+        slider.enable()
+
+def in_slider_change(e):
+    if direction.value != 'Równa ważność':
+        state['value'] = int(e.value)
+        value_badge.set_text(str(state['value']))
 
 # ======= UI ======
 
@@ -65,18 +88,18 @@ with ui.card().classes('main-card'):
 
         direction = ui.radio(
             ['Lewe kryterium ważniejsze', 'Równa ważność', 'Prawe kryterium ważniejsze'],
-            value='Równa ważność'
+            value='Lewe kryterium ważniejsze',
+            on_change = on_direction_change
         ).classes('direction-radio')
 
     ui.label('Wskaż, które kryterium jest ważniejsze, a następnie określ siłę przewagi').classes('introduction')
-    value_badge = ui.label('5').classes('slider-value')
+    value_badge = ui.label('25').classes('slider-value')
 
-    slider = ui.slider(min=1, max=9, value=5, step=1).classes('custom-slider')
+    slider = ui.slider(min=2, max=9, value=2, step=1).classes('custom-slider')
     def update_badge(e):
         value_badge.set_text(e.value)
     slider.on_value_change(update_badge)
     with ui.row().classes('slider-labels'):
-        ui.label('1')
         ui.label('2')
         ui.label('3')
         ui.label('4')
